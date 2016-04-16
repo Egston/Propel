acf0cd466ca3cf9bfd96cb907f88d78e3f8c8309
Petr Gajdůšek <gajdusek.petr@centrum.cz>
2016-04-16 19:55:58 +0200

    Fix and improve script test/reset_tests.sh
    - Fix fixtures build/ directories were not removed before rebuilding.
    
    - Don't try to rebuild fixtures directories which are missing
      build.properties file.
    
    - Improve detection of directories locations
      (Propel project root and test/fixtures/);
      reset_tests.sh may now be invoked from anywhere.
    
    - Fix case when absolute path to the Propel project directory
      contains spaces.
    
    - Optional verbosity levels:
      + Option -v prints propel-gen commands being executed.
      + Options -vv prints also commands output.
    
    - Help (-h|--help)
    
    - Slightly improved output for "reverse fixture".

--- test/reset_tests.sh
+++ test/reset_tests.sh
@@ -2,35 +2,56 @@
 # Reset Propel tests fixtures
 # 2011 - William Durand <william.durand1@gmail.com>
 
-CURRENT=`pwd`
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+cat <<EOT
+$(basename $0) [OPTIONS]
+Reset Propel tests fixtures
+
+Options:
+    -v      Be verbose: Prints propel-gen commands being executed.
+    -vv     Be more verbose: Prints propel-gen commands output.
+EOT
+    exit
+fi
+
+VERBOSE=$(echo "$*" | grep -Ewo -e '-v+' | cut -d- -f2 | tr -d '\n' | wc -c)
+
+FIXTURES_DIR="$(dirname $0)/fixtures";
+ROOT_DIR="$(realpath -e "$(dirname $0)/..")"
+
+if [ ! -d "$FIXTURES_DIR" ] ; then
+    echo "ERROR: {$FIXTURES_DIR} directory not found !" >&2
+    exit 1
+fi
+if [ -z "$ROOT_DIR" ] ; then
+    echo "ERROR: Propel1 root directory not found !" >&2
+    exit 1
+fi
+
+function gen
+{
+    local cmd="$ROOT_DIR/generator/bin/propel-gen $*"
+    [ $VERBOSE -ge 1 ] && echo "    $cmd"
+    [ $VERBOSE -ge 2 ] && $cmd || $cmd >/dev/null
+}
 
 function rebuild
 {
     local dir=$1
 
-    echo "[ $dir ]"
-
-    if [ -d "$dir/build" ] ; then
-        rm -rf "$dir/build"
+    if [ ! -f "$FIXTURES_DIR/$dir/build.properties" ]; then
+        return
     fi
 
-    $ROOT/generator/bin/propel-gen $FIXTURES_DIR/$dir main > /dev/null
-    $ROOT/generator/bin/propel-gen $FIXTURES_DIR/$dir insert-sql > /dev/null
+    echo "[ $dir ]"
+
+    if [ -d "$FIXTURES_DIR/$dir/build" ] ; then
+        rm -rf "$FIXTURES_DIR/$dir/build"
+    fi
+
+    gen $FIXTURES_DIR/$dir main
+    gen $FIXTURES_DIR/$dir insert-sql
 }
-
-ROOT_DIR=""
-FIXTURES_DIR=""
-
-if [ -d "$CURRENT/fixtures" ] ; then
-    ROOT=".."
-    FIXTURES_DIR="$CURRENT/fixtures"
-elif [ -d "$CURRENT/test/fixtures" ] ; then
-    ROOT="."
-    FIXTURES_DIR="$CURRENT/test/fixtures"
-else
-    echo "ERROR: No 'test/fixtures/' directory found !"
-    exit 1
-fi
 
 DIRS=`ls $FIXTURES_DIR`
 
@@ -44,7 +65,7 @@ REVERSE_DIRS=`ls $FIXTURES_DIR/reverse`
 
 for dir in $REVERSE_DIRS ; do
     if [ -f "$FIXTURES_DIR/reverse/$dir/build.properties" ] ; then
-        echo "[ $dir ]"
-        $ROOT/generator/bin/propel-gen $FIXTURES_DIR/reverse/$dir insert-sql > /dev/null
+        echo "[ reverse / $dir ]"
+        gen $FIXTURES_DIR/reverse/$dir insert-sql
     fi
 done
