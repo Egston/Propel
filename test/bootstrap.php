31f758099a4f7a084930325dc6b964afdb1b6a15
Petr Gajdůšek <gajdusek.petr@centrum.cz>
2016-05-09 05:05:43 +0200

    [test] Explicitly set error reporting to E_ALL | E_STRICT
    So test results do not depend on PHP error level setting.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 if (file_exists($file = dirname(__FILE__) . '/../vendor/autoload.php')) {
     set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../vendor/phing/phing/classes');
 
